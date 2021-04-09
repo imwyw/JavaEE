2ef677bdf379899414c1f7cb21d6920cd309a13a
imwyw <now_way@126.com>
2021-04-09 11:12:05 +0800

    服务提供者注册进入EurekaServer

--- src-demo/cloud-demo/cloud-provider-payment8001/src/main/java/com/imwyw/springcloud/PaymentMain.java
+++ src-demo/cloud-demo/cloud-provider-payment8001/src/main/java/com/imwyw/springcloud/PaymentMain.java
@@ -2,6 +2,7 @@ package com.imwyw.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 20009439
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/4/8 14:56
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain.class, args);
