49ed10cfc98360e19e44dc9ab76583231d918229
imwyw <now_way@126.com>
2021-04-09 15:05:01 +0800

    注册多个服务：order服务

--- src-demo/cloud-demo/cloud-consumer-order8101/src/main/java/com/imwyw/springcloud/OrderMain.java
+++ src-demo/cloud-demo/cloud-consumer-order8101/src/main/java/com/imwyw/springcloud/OrderMain.java
@@ -2,6 +2,7 @@ package com.imwyw.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 20009439
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/4/8 16:57
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class, args);
