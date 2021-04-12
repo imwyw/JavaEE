d25692d077b000862a141c1c934245624ffe77eb
imwyw <now_way@126.com>
2021-04-12 15:55:58 +0800

    增加服务注册，获取服务信息

--- src-demo/cloud-demo/cloud-provider-payment8001/src/main/java/com/imwyw/springcloud/controller/PaymentController.java
+++ src-demo/cloud-demo/cloud-provider-payment8001/src/main/java/com/imwyw/springcloud/controller/PaymentController.java
@@ -3,14 +3,18 @@ package com.imwyw.springcloud.controller;
 import com.imwyw.springcloud.entities.CommonResult;
 import com.imwyw.springcloud.entities.Payment;
 import com.imwyw.springcloud.service.PaymentService;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author 20009439
@@ -27,6 +31,12 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    /**
+     * 服务发现注册端，注意包名：org.springframework.cloud.client.discovery.DiscoveryClient
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(Payment payment) {
@@ -48,6 +58,24 @@ public class PaymentController {
         } else {
             return new CommonResult(500, "没有对应记录");
         }
+    }
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("****service:" + service + "****");
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances(applicationName);
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t"
+                    + instance.getPort() + "\t" + instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 }
