da61a056105d952454b8a55f776bd782f720234a
imwyw <now_way@126.com>
2021-04-13 10:34:51 +0800

    openfeign 自动负载均衡在客户端服务中的接口对应和调用；

---
+++ src-demo/cloud-demo/cloud-consumer-feign-order8102/src/main/java/com/imwyw/springcloud/OrderFeignMain.java
@@ -0,0 +1,20 @@
+package com.imwyw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author 20009439
+ * @title: OrderFeignMain
+ * @projectName cloud-demo
+ * @description: TODO
+ * @date 2021/4/13 9:53
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain.class, args);
+    }
+}
---
+++ src-demo/cloud-demo/cloud-consumer-feign-order8102/src/main/java/com/imwyw/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,33 @@
+package com.imwyw.springcloud.service;
+
+import com.imwyw.springcloud.entities.CommonResult;
+import com.imwyw.springcloud.entities.Payment;
+import feign.Param;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author 20009439
+ * @title: PaymentFeignService
+ * @projectName cloud-demo
+ * @description: TODO
+ * @date 2021/4/13 9:56
+ *
+ * OpenFeign 自带负载均衡调用，标记对应微服务名称，新增方法签名一致，会自动进行负载均衡
+ *
+ * FeignClient value 是 eureka 服务名称
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    /**
+     * 对应provider微服务中controller api
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
