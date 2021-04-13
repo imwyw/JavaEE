9ea7de06f4f3f49e15d3d33f00b4ed79d556719f
imwyw <now_way@126.com>
2021-04-13 16:49:08 +0800

    服务消费方增加hystrix降级操作

--- src-demo/cloud-demo/cloud-consumer-feign-hystrix-order8101/src/main/java/com/imwyw/springcloud/OrderHystrixMain.java
+++ src-demo/cloud-demo/cloud-consumer-feign-hystrix-order8101/src/main/java/com/imwyw/springcloud/OrderHystrixMain.java
@@ -2,6 +2,7 @@ package com.imwyw.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,9 +11,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @projectName cloud-demo
  * @description: 描述
  * @date 2021/4/13 15:26
+ *
+ * 通常都是在服务客户端进行 hystrix断路器 降级操作
+ *
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
 public class OrderHystrixMain {
 
     public static void main(String[] args) {
--- src-demo/cloud-demo/cloud-consumer-feign-hystrix-order8101/src/main/java/com/imwyw/springcloud/controller/OrderHystrixController.java
+++ src-demo/cloud-demo/cloud-consumer-feign-hystrix-order8101/src/main/java/com/imwyw/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.imwyw.springcloud.controller;
 
 import com.imwyw.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,10 +36,18 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            // 3秒钟以内就是正常的业务逻辑，超时就降级
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         log.info("*******result:" + result);
         return result;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "我是消费服务8101，对方支付服务繁忙或异常，请稍后再试。。。";
     }
 }
 
