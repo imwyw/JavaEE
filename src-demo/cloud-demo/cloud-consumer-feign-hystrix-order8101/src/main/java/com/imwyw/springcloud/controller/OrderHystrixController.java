7dca297e871dfaa60cdb469b53c86ab4251b84e9
imwyw <now_way@126.com>
2021-04-13 20:56:42 +0800

    hystrix 服务消费方降级操作

--- src-demo/cloud-demo/cloud-consumer-feign-hystrix-order8101/src/main/java/com/imwyw/springcloud/controller/OrderHystrixController.java
+++ src-demo/cloud-demo/cloud-consumer-feign-hystrix-order8101/src/main/java/com/imwyw/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.imwyw.springcloud.controller;
 
 import com.imwyw.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -17,9 +18,12 @@ import javax.annotation.Resource;
  * @projectName cloud-demo
  * @description: 描述
  * @date 2021/4/13 15:45
+ * 
+ * DefaultProperties 设置服务降级的统一处理方法
  */
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystrixController {
 
     @Resource
@@ -36,11 +40,13 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
-            // 3秒钟以内就是正常的业务逻辑，超时就降级
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+//            // 3秒钟以内就是正常的业务逻辑，超时就降级
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+//    })
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        int res = 1 / 0;
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         log.info("*******result:" + result);
         return result;
@@ -48,6 +54,15 @@ public class OrderHystrixController {
 
     public String paymentInfo_TimeOutHandler(Integer id) {
         return "我是消费服务8101，对方支付服务繁忙或异常，请稍后再试。。。";
+    }
+
+    /**
+     * 全局 fallback 方法
+     *
+     * @return
+     */
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息，请稍后再试。。。。";
     }
 }
 
