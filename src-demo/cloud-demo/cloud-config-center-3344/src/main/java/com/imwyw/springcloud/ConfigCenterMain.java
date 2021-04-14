2525ab34c7904708a253aa16045a31eda2c0c6d4
imwyw <now_way@126.com>
2021-04-14 15:39:12 +0800

    添加 SpringCloud config 中心配置

---
+++ src-demo/cloud-demo/cloud-config-center-3344/src/main/java/com/imwyw/springcloud/ConfigCenterMain.java
@@ -0,0 +1,25 @@
+package com.imwyw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wangyuanwei
+ * @title: ConfigCenterMain
+ * @projectName cloud-demo
+ * @description: 描述
+ * @date 2021/4/14 14:28
+ *
+ * 如下地址可以访问github上yml配置
+ * http://localhost:3344/master/config-dev.yml
+ * http://localhost:3344/config-dev/master
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain.class, args);
+    }
+}
