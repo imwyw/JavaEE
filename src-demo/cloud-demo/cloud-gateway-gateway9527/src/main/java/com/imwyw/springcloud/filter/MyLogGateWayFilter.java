b415841e81725f4613706a3c22b4f53072a6defb
imwyw <now_way@126.com>
2021-04-14 13:33:53 +0800

    全局 gateway 进行filter过滤

---
+++ src-demo/cloud-demo/cloud-gateway-gateway9527/src/main/java/com/imwyw/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,53 @@
+package com.imwyw.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @author wangyuanwei
+ * @title: MyLogGateWayFilter
+ * @projectName cloud-demo
+ * @description: 全局的 gateway filter 访问必须附加 uname ，否则不被接受
+ * @date 2021/4/14 13:21
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("****come in MyLogGateWayFilter:  " + new Date());
+
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            log.info("****用户名为null，非法用户！");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    /**
+     * Get the order value of this object.
+     * <p>Higher values are interpreted as lower priority. As a consequence,
+     * the object with the lowest value has the highest priority (somewhat
+     * analogous to Servlet {@code load-on-startup} values).
+     * <p>Same order values will result in arbitrary sort positions for the
+     * affected objects.
+     *
+     * @return the order value
+     * @see #HIGHEST_PRECEDENCE
+     * @see #LOWEST_PRECEDENCE
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
